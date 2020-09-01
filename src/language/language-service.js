7a0b67f46f3d8ba2f466cc430e893aefe20338ac
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-01 10:24:06 -0400

    language services add

--- src/language/language-service.js
+++ src/language/language-service.js
@@ -1,33 +1,49 @@
 const LanguageService = {
-  getUsersLanguage(db, user_id) {
-    return db
-      .from('language')
-      .select(
-        'language.id',
-        'language.name',
-        'language.user_id',
-        'language.head',
-        'language.total_score',
-      )
-      .where('language.user_id', user_id)
-      .first()
-  },
+	getUsersLanguage(db, user_id) {
+		return db
+			.from('language')
+			.select(
+				'language.id',
+				'language.name',
+				'language.user_id',
+				'language.head',
+				'language.total_score'
+			)
+			.where('language.user_id', user_id)
+			.first();
+	},
 
-  getLanguageWords(db, language_id) {
-    return db
-      .from('word')
-      .select(
-        'id',
-        'language_id',
-        'original',
-        'translation',
-        'next',
-        'memory_value',
-        'correct_count',
-        'incorrect_count',
-      )
-      .where({ language_id })
-  },
-}
+	getLanguageWords(db, language_id) {
+		return db
+			.from('word')
+			.select(
+				'id',
+				'language_id',
+				'original',
+				'translation',
+				'next',
+				'memory_value',
+				'correct_count',
+				'incorrect_count'
+			)
+			.where({ language_id });
+	},
+	getLanguageHead(db, head) {
+		return db
+			.from('word')
+			.select(
+				'id',
+				'language_id',
+				'original',
+				'translation',
+				'next',
+				'memory_value',
+				'correct_count',
+				'incorrect_count'
+			)
+			.where({ id: head })
+			.first();
+	},
+};
 
-module.exports = LanguageService
+module.exports = LanguageService;
