0468f173a5183ad1974ef7df5c2298b6587dffdf
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-01 15:31:39 -0400

    update word table for svg column

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -21,18 +21,18 @@ VALUES
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next", "hex", "script")
 VALUES
-  (1, 1, 'Oranji', 'orange', 2, '#FFB74D', 'src/img/script/orange.svg'),
-  (2, 1, 'Ki-iro', 'yellow', 3, '#FFF07F', 'src/img/script/yellow.svg'),
-  (3, 1, 'Ao', 'blue', 4, '#84B3FD', 'src/img/script/blue.svg'),
-  (4, 1, 'Aka', 'red', 5, '#B51F23', 'src/img/script/red.svg'),
-  (5, 1, 'Midori', 'green', 6, '#6FEFB0', 'src/img/script/green.svg'),
-  (6, 1, 'Kuro', 'black', 7, '#000000', 'src/img/script/black.svg'),
-  (7, 1, 'Cha-iro', 'brown', 8, '#8D6E64', 'src/img/script/brown.svg'),
-  (8, 1, 'Pinku', 'pink', 9, '#FD4482', 'src/img/script/pink.svg'),
-  (9, 1, 'Murasaki', 'purple', 10, '#BA68C8', 'src/img/script/purple.svg'),
-  (10, 1, 'Shiro', 'white', 11, '#FFFFFF', 'src/img/script/white.svg'),
-  (11, 1, 'Hai-iro', 'gray', 12, '#BDBDBD', 'src/img/script/gray.svg'),
-  (12, 1, 'Rairakku', 'lilac', null, '#C8A2C8', 'src/img/script/lilac.svg');
+  (1, 1, 'Oranji', 'orange', 2, '#FFB74D', 'orange.svg'),
+  (2, 1, 'Ki-iro', 'yellow', 3, '#FFF07F', 'yellow.svg'),
+  (3, 1, 'Ao', 'blue', 4, '#84B3FD', 'blue.svg'),
+  (4, 1, 'Aka', 'red', 5, '#B51F23', 'red.svg'),
+  (5, 1, 'Midori', 'green', 6, '#6FEFB0', 'green.svg'),
+  (6, 1, 'Kuro', 'black', 7, '#000000', 'black.svg'),
+  (7, 1, 'Cha-iro', 'brown', 8, '#8D6E64', 'brown.svg'),
+  (8, 1, 'Pinku', 'pink', 9, '#FD4482', 'pink.svg'),
+  (9, 1, 'Murasaki', 'purple', 10, '#BA68C8', 'purple.svg'),
+  (10, 1, 'Shiro', 'white', 11, '#FFFFFF', 'white.svg'),
+  (11, 1, 'Hai-iro', 'gray', 12, '#BDBDBD', 'gray.svg'),
+  (12, 1, 'Rairakku', 'lilac', null, '#C8A2C8', 'lilac.svg');
   
 
 UPDATE "language" SET head = 1 WHERE id = 1;
