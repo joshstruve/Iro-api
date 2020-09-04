6306a16d4988ab40129344cf782a8f5281921fd8
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-04 18:28:44 -0400

    ready to deploy #2

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
 	DB_URL:
-		process.env.DB_URL ||
+		process.env.DATABASE_URL ||
 		'postgresql://dunder_mifflin@localhost/spaced-repetition',
 	JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
 	JWT_EXPIRY: process.env.JWT_EXPIRY || '3h',
