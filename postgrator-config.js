2919daa9b0ae08bb836f540068f4d15e6a3d57f7
Matt <63121708+themlp101@users.noreply.github.com>
2020-09-07 10:08:53 -0400

    deploy changes

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,12 +3,17 @@ require('dotenv').config()
 module.exports = {
 	migrationDirectory: 'migrations',
 	driver: 'pg',
-	host: process.env.MIGRATION_DB_HOST,
-	port: process.env.MIGRATION_DB_PORT,
-	database:
+	// host: process.env.MIGRATION_DB_HOST,
+	// port: process.env.MIGRATION_DB_PORT,
+	// database:
+	// 	process.env.NODE_ENV === 'test'
+	// 		? process.env.MIGRATION_DB_NAME_TEST
+	// 		: process.env.MIGRATION_DB_NAME,
+	// username: process.env.MIGRATION_DB_USER,
+	// password: process.env.MIGRATION_DB_PASS,
+	connectionString:
 		process.env.NODE_ENV === 'test'
-			? process.env.MIGRATION_DB_NAME_TEST
-			: process.env.MIGRATION_DB_NAME,
-	username: process.env.MIGRATION_DB_USER,
-	password: process.env.MIGRATION_DB_PASS,
+			? process.env.TEST_DATABASE_URL
+			: process.env.DATABASE_URL,
+	// ssl: !!process.env.ssl,
 }
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	PORT: process.env.PORT || 8000,
 	NODE_ENV: process.env.NODE_ENV || 'development',
-	DB_URL:
+	DATABASE_URL:
 		process.env.DATABASE_URL ||
 		'postgresql://dunder_mifflin@localhost/spaced-repetition',
 	JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
